01ddf41e58fbc5ecc8aea68eace5f35164d35f6f
loong.woo <longwunet@outlook.com>
2024-05-20 03:58:05 +0000

    feat: 更新 ts-lint

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-export default [{
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended'
-	],
-	rules: {
-		'prettier/prettier': 'warn'
-	}
-}];
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+// export default tseslint.config([{
+// 	parser: '@typescript-eslint/parser',
+// 	plugins: ['@typescript-eslint'],
+// 	extends: [
+// 		'eslint:recommended',
+// 		'plugin:@typescript-eslint/recommended',
+// 		'plugin:prettier/recommended'
+// 	],
+// 	rules: {
+// 		'prettier/prettier': 'warn'
+// 	}
+// }]);
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended
+)
